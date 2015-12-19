77ed67c188bf62d94dc637ec5528b364e73c2b88
Gavin Yang <gavin.yang@me.com>
2015-12-19 15:54:22 +0800

    init

---
+++ src/main/java/com/github/redexpress/jsonxml/JsonXml.java
@@ -0,0 +1,32 @@
+package com.github.redexpress.jsonxml;
+
+import net.sf.json.JSONObject;
+import net.sf.json.xml.XMLSerializer;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class JsonXml {
+
+	public static String xml2json(String xml) {
+		return new XMLSerializer().read(xml).toString();
+	}
+
+	public static String json2xml(String json) {
+		JSONObject jobj = JSONObject.fromObject(json);
+		return new XMLSerializer().write(jobj);
+	}
+	
+	public static String findStringFromJsonByJsonPath(String json, String jsonPath){
+		Object object = JsonPath.read(json, jsonPath);
+		return object.toString();
+	}
+	
+	public static String findStringFromJsonByXpath(String json, String xpath){
+		String xml = JsonXml.json2xml(json);
+		return xml;
+	}
+	
+	public static String findStringFromXmlByXpath(String xml, String xpath) {
+		return xml;
+	}
+}
